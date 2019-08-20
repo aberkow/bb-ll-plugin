e9483cd5f7a86396e13894f3a374dcebb9612df2
Berkowitz <adb16130@d71h68.pr.uconn.edu>
2019-08-20 08:27:38 -0400

    removed enqueue style function

--- lib/AssetLoader.php
+++ lib/AssetLoader.php
@@ -4,7 +4,6 @@ namespace BBLL\Lib;
 
 class AssetLoader {
   public static function enqueue_scripts() {
-    // wp_enqueue_style('bb-ll', BB_LL_URL . 'build/css/main.css');
     wp_enqueue_script('bb-ll', BB_LL_URL . 'build/js/index.js', array(), null, true);
   }
 }
