3d7b1ed7258bfd2bc0edd04e98e43a2d08ec32c0
Berkowitz <adb16130@d71h68.pr.uconn.edu>
2019-09-12 10:33:32 -0400

    fixed build script to run gulp better

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-composer install --no-dev
-export NODE_ENV=production
+echo "begin npm install"
 npm install
+echo "end npm install"
+
+# set the env after installing 
+# otherwise, npm only installs deps and not devDeps
+echo "set node env"
+export NODE_ENV=production
+echo "end node env $NODE_ENV"
+
+echo "begin gulp"
 gulp
+echo "end gulp"
